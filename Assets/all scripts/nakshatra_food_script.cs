5677d57b7ae2b25769627f5552dd4fc61a28d2ef
radagon <saswatskar@gmail.com>
2026-04-19 15:32:08 +0530

    added velocity based movement

--- Assets/all scripts/nakshatra_food_script.cs
+++ Assets/all scripts/nakshatra_food_script.cs
@@ -1,16 +1,29 @@
+using Unity.Collections;
 using UnityEngine;
 
 public class nakshatra_food_script : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private float velocity_change_clock;
+    public Vector3 velocity;
+    public float velocity_normalisation;
+    public float change_timing;
     void Start()
     {
-        
+        transform.position = generate_random_vector3();
+        velocity = generate_random_vector3()/velocity_normalisation;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += velocity*Time.deltaTime;
+    }
+    Vector3 generate_random_vector3()
+    {
+        return new Vector3(
+            (float)Random.Range(-600 , 600)/100,
+            (float)Random.Range(-300 , 300)/100,
+            0);
     }
 }
