ac67b9415e4482e5e86c32c331456ba598e9365c
radagon <saswatskar@gmail.com>
2026-05-03 11:02:00 +0530

    added switch cases for nakshatra AI

--- Assets/all scripts/nakshatra_food_script.cs
+++ Assets/all scripts/nakshatra_food_script.cs
@@ -13,6 +13,8 @@ public class nakshatra_food_script : MonoBehaviour
     public float speed;
     public float turn_speed = 0.5f;
     public int clock = 0;
+
+    public int mode;
     void Start()
     {
         transform.position = HelperFunctions.generate_random_vector3();
@@ -27,22 +29,36 @@ public class nakshatra_food_script : MonoBehaviour
 
     void FixedUpdate()
     {
+
         Vector3 target_vector = transform.position-TargetObject.transform.position ;
-        Vector3 new_direction = Vector3.RotateTowards(
-            rb.linearVelocity,
-            target_vector,
-            turn_speed,
-            0f
-        );
-        clock += 1;
-        if (clock >= 100)
+
+        if (target_vector.magnitude < 9){mode = 1;}//avoiding mode
+        else mode = 0;
+        switch (mode)
         {
-            new_direction += HelperFunctions.generate_random_vector3()*50;
-            clock = 0;
+            case 1 ://escape mode
+                Vector3 new_direction = Vector3.RotateTowards(
+                    rb.linearVelocity,
+                    target_vector,
+                    turn_speed,
+                    0f
+                );
+                clock += 1;
+                if (clock >= 100)
+                {
+                    new_direction += HelperFunctions.generate_random_vector3()*50;
+                    clock = 0;
+                }
+                new_direction.Normalize();   
+                new_direction = new_direction*speed*UnityEngine.Random.Range(1 , 3);
+                rb.linearVelocity = new_direction;
+                break;
+            
+            default : 
+                rb.linearVelocity = Vector2.zero;
+                break;
+
         }
-        new_direction.Normalize();   
-        new_direction = new_direction*speed*UnityEngine.Random.Range(1 , 3);
-        rb.linearVelocity = new_direction;
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
