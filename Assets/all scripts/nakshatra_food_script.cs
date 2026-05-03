6815753685920a523fa651961ff8b69d95066330
radagon <saswatskar@gmail.com>
2026-05-03 12:38:10 +0530

    added avoid modes

--- Assets/all scripts/nakshatra_food_script.cs
+++ Assets/all scripts/nakshatra_food_script.cs
@@ -15,6 +15,7 @@ public class nakshatra_food_script : MonoBehaviour
     public int clock = 0;
 
     public int mode;
+    public int avoid_mode;
     void Start()
     {
         transform.position = HelperFunctions.generate_random_vector3();
@@ -32,7 +33,23 @@ public class nakshatra_food_script : MonoBehaviour
 
         Vector3 target_vector = transform.position-TargetObject.transform.position ;
 
-        if (target_vector.magnitude < 9){mode = 1;}//avoiding mode
+        if (target_vector.magnitude < 9)
+        {
+            mode = 1;//avoiding mode
+
+            if (clock%200 <70)
+            {
+                mode = 2;
+                if (clock%200 == 90)
+                {
+                    if (UnityEngine.Random.Range(0, 2) == 1)
+                    {
+                        avoid_mode = 1;
+                    }
+                    else{avoid_mode = 0;}
+                }
+            }        
+        }
         else mode = 0;
         switch (mode)
         {
@@ -43,22 +60,41 @@ public class nakshatra_food_script : MonoBehaviour
                     turn_speed,
                     0f
                 );
-                clock += 1;
-                if (clock >= 100)
-                {
-                    new_direction += HelperFunctions.generate_random_vector3()*50;
-                    clock = 0;
-                }
                 new_direction.Normalize();   
                 new_direction = new_direction*speed*UnityEngine.Random.Range(1 , 3);
                 rb.linearVelocity = new_direction;
                 break;
             
+            case 2 : //juke mode
+                Vector3 juke_direction;
+                if(avoid_mode == 1)
+                {
+                    juke_direction = Vector3.RotateTowards(
+                    rb.linearVelocity,
+                    new Vector3(-target_vector.y,target_vector.x,0f),
+                    turn_speed,
+                    0f
+                    );
+                }
+                else
+                {
+                        juke_direction = Vector3.RotateTowards(
+                        rb.linearVelocity,
+                        new Vector3(target_vector.y,-target_vector.x,0f),
+                        turn_speed,
+                        0f);
+                }
+                
+                juke_direction.Normalize();
+                juke_direction = juke_direction*speed*UnityEngine.Random.Range(3 , 5);
+                rb.linearVelocity = juke_direction;
+                break;
             default : 
                 rb.linearVelocity = Vector2.zero;
                 break;
-
         }
+
+        clock++;
     }
     void OnCollisionEnter2D(Collision2D collision)
     {
