d11a3b8eb81aba93d7c008b51972ac7cf810591e
radagon <saswatskar@gmail.com>
2026-06-05 13:10:42 +0530

    added camera zoom
    extra_commit

--- Assets/all scripts/characters/goo_script.cs
+++ Assets/all scripts/characters/goo_script.cs
@@ -7,6 +7,7 @@ public class goo_script : Character
     public float moveSpeed;
     private Vector2 _moveDirection;
     private float _density_change;
+    [SerializeField] float scale;
 
     public InputActionReference move;
     public InputActionReference density_control;
@@ -20,13 +21,17 @@ public class goo_script : Character
         transform.position +=(Vector3)_moveDirection*moveSpeed*Time.deltaTime;
         _density_change = density_control.action.ReadValue<float>();
         density += _density_change*Time.deltaTime;
-        float scale = 0.136f * Mathf.Sqrt((float)mass / density);
+        scale = 0.136f * Mathf.Sqrt((float)mass / density);
         transform.localScale = Vector3.one * scale;
         density = Mathf.Clamp(density, 0.5f, 1.5f);
     }
 
     public Vector2 get_moveDirection()
-    {
+    { 
         return _moveDirection;
+    }
+    public float get_scale()
+    {
+        return scale;
     }
 }
